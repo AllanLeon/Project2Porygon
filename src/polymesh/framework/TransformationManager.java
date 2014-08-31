0e06093bf9f2d6ed0266743cd1cd52031868f54a
Allan Leon <allan.leon@outlook.com>
2014-08-31 19:00:06 -0400

    Created rotation method

--- src/polymesh/framework/TransformationManager.java
+++ src/polymesh/framework/TransformationManager.java
@@ -19,4 +19,44 @@ public class TransformationManager {
 		}
 		return res;
 	}
+	
+	private static int[][] toHomogeneousCoordinates(int[][] m) {
+		int mRows = m.length;
+		int mColumns = m[0].length;
+		int res[][] = new int[mRows + 1][mColumns + 1];
+		for (int i = 0; i < mRows; i++) {
+			for (int j = 0; j < mColumns; j++) {
+				res[i][j] = m[i][j];
+			}
+			res[i][mColumns] = 0;
+		}
+		for (int j = 0; j < mColumns; j++) {
+			res[mRows][j] = 0;
+		}
+		res[mRows][mColumns] = 1;
+		return res;
+	}
+	
+	public static int[][] rotation3D(int[] axis, int angle) {
+		int[][] res = new int[3][3];
+		if (axis.length == 3) {
+			int nx = axis[0];
+			int ny = axis[1];
+			int nz = axis[2];
+			double cos = Math.cos(angle);
+			double sin = Math.cos(angle);
+			res[0][0] = (int) Math.round(nx*nx*(1 - cos) + cos);
+			res[1][0] = (int) Math.round(nx*ny*(1 - cos) - nz*sin);
+			res[2][0] = (int) Math.round(nx*nz*(1 - cos) + ny*sin);
+			
+			res[0][1] = (int) Math.round(nx*ny*(1 - cos) + nz*sin);
+			res[1][1] = (int) Math.round(ny*ny*(1 - cos) + cos);
+			res[2][1] = (int) Math.round(ny*nz*(1 - cos) - nx*sin);
+			
+			res[0][2] = (int) Math.round(nx*nz*(1 - cos) - ny*sin);
+			res[1][2] = (int) Math.round(ny*nz*(1 - cos) + nx*sin);
+			res[2][2] = (int) Math.round(nz*nz*(1 - cos) + cos);
+		}
+		return toHomogeneousCoordinates(res);
+	}
 }
