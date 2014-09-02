68def1990bed7d9ecba394d5311cb5b51b74c993
Allan Leon <allan.leon@outlook.com>
2014-09-02 00:04:40 -0400

    Added edges and corners lists

--- src/polymesh/main/Polymesh.java
+++ src/polymesh/main/Polymesh.java
@@ -7,17 +7,37 @@ import java.util.List;
 public class Polymesh {
 
 	private List<Polygon> polygons;
+	private List<Edge> edges;
+	private List<Point> corners;
 	
 	public Polymesh() {
 		polygons = new ArrayList<Polygon>();
+		edges = new ArrayList<Edge>();
+		corners = new ArrayList<Point>();
 	}
 	
 	public List<Polygon> getPolygons() {
 		return polygons;
 	}
 	
+	public List<Polygon> getEdges() {
+		return polygons;
+	}
+	
+	public List<Polygon> getCorners() {
+		return polygons;
+	}
+	
 	public void addPolygon(Polygon polygon) {
 		polygons.add(polygon);
+	}
+	
+	public void addEdge(Edge edge) {
+		edges.add(edge);
+	}
+	
+	public void addCorner(Point corner) {
+		corners.add(corner);
 	}
 	
 	public void draw(Graphics g) {
