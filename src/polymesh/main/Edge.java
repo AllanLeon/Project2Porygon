2411e476253bde645a64f08cdefce3a8b529d7cd
Allan Leon <allan.leon@outlook.com>
2014-09-07 21:19:28 -0400

    Added color to edges

--- src/polymesh/main/Edge.java
+++ src/polymesh/main/Edge.java
@@ -7,17 +7,26 @@ import java.awt.Graphics;
 
 public class Edge {
 
-	public Point initPoint;
-	public Point finalPoint;
+	private Point initPoint;
+	private Point finalPoint;
+	private Color color;
 	
 	public Edge(int x0, int y0, int z0, int x1, int y1, int z1) {
 		this.initPoint = new Point(x0, y0, z0);
 		this.finalPoint = new Point(x1, y1, z1);
+		this.color = Color.CYAN;
 	}
 
 	public Edge(Point A, Point B) {
 		this.initPoint = A;
 		this.finalPoint = B;
+		this.color = Color.CYAN;
+	}
+	
+	public Edge(Point A, Point B, Color color) {
+		this.initPoint = A;
+		this.finalPoint = B;
+		this.color = color;
 	}
 
 	public void draw(Graphics g) {
@@ -32,7 +41,7 @@ public class Edge {
 		y0 = Main.BIG_PANEL_HEIGHT / 2 - y0;
 		x1 += Main.BIG_PANEL_WIDTH / 2;
 		y1 = Main.BIG_PANEL_HEIGHT / 2 - y1;
-		Drawer.drawLine(g, x0, y0, x1, y1, Color.CYAN);
+		Drawer.drawLine(g, x0, y0, x1, y1, color);
 	}
 	
 	public void drawFrontal(Graphics g) {
@@ -44,7 +53,7 @@ public class Edge {
 		y0 = Main.SMALL_PANEL_HEIGHT / 2 - y0;
 		x1 += Main.SMALL_PANEL_WIDTH / 2;
 		y1 = Main.SMALL_PANEL_HEIGHT / 2 - y1;
-		Drawer.drawLine(g, x0, y0, x1, y1, Color.CYAN);
+		Drawer.drawLine(g, x0, y0, x1, y1, color);
 	}
 	
 	public void drawSide(Graphics g) {
@@ -56,7 +65,7 @@ public class Edge {
 		y0 = Main.SMALL_PANEL_HEIGHT / 2 - y0;
 		x1 += Main.SMALL_PANEL_WIDTH / 2 - 40;
 		y1 = Main.SMALL_PANEL_HEIGHT / 2 - y1;
-		Drawer.drawLine(g, x0, y0, x1, y1, Color.CYAN);
+		Drawer.drawLine(g, x0, y0, x1, y1, color);
 	}
 	
 	public void drawTop(Graphics g) {
@@ -68,6 +77,6 @@ public class Edge {
 		y0 = Main.SMALL_PANEL_HEIGHT / 2 - y0 + 40;
 		x1 += Main.SMALL_PANEL_WIDTH / 2;
 		y1 = Main.SMALL_PANEL_HEIGHT / 2 - y1 + 40;
-		Drawer.drawLine(g, x0, y0, x1, y1, Color.CYAN);
+		Drawer.drawLine(g, x0, y0, x1, y1, color);
 	}
 }
